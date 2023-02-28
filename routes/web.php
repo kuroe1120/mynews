fb401d59b37a44f174bba46daccb20106047d386
kuroe1120 <kuroe0720@i.softbank.jp>
2023-02-28 15:08:46 +0000

    課題29

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'edit');
+});
